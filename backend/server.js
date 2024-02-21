ff3337c15de74436559da087ee81d340370ccd30
Karthick <ganeshkarthik18697@gmail.com>
2024-02-21 13:54:31 +0530

    need rest

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
-
 const express = require('express');
 const app = express();
 const port = 8000 ;
 
 
+// middlewares
+app.use(express.json())
 
+
+// routes
 app.get("/",(req,res)=>{
     res.send("i'm server - from backend ")
 })
 
-
-
+// 404 page
 app.use((req,res)=>{
     res.send("404 page not found !");
 })
