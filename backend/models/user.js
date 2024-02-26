4421d77785d153969b865a2b295aa99005d74f6c
Karthick <ganeshkarthik18697@gmail.com>
2024-02-27 00:29:28 +0530

    solving git error

--- backend/models/user.js
+++ backend/models/user.js
@@ -41,6 +41,7 @@ userSchema.statics.signup = async function(email,password) {
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password,salt);
   const user = await this.create({email,password:hash});
+  
 
   return user;
 
